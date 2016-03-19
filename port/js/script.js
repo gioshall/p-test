5d3d8a2ae2c96c12f828379e8396f1add186d136
gio <giogioshall@yahoo.com.tw>
2016-03-19 15:48:50 +0800

    okok

--- port/js/script.js
+++ port/js/script.js
@@ -2,21 +2,25 @@
 var wHeight = $(window).height();
 var wWidth = $(window).width();
 
-$('header').css('height', wHeight);
-if ( (wHeight / wWidth) < (9/16) ) {
-    $('header video').css('width','100%');
-} else {
-    $('header video').css('height','100%')
-};
+// $('header').css('height', wHeight);
+// if ( (wHeight / wWidth) < (9/16) ) {
+//     $('header video').css('width','100%');
+// } else {
+//     $('header video').css('height','100%')
+// };
 
-$( window ).resize(function() {
-    $('header video').attr('style','')
-    $('header').css('height', $(window).height());
-    if ( ($(window).height() / $(window).width()) < (9/16) ) {
-      $('header video').css('width','100%')
-    } else {
-      $('header video').css('height','100%')
-    };
+// $( window ).resize(function() {
+//     $('header video').attr('style','')
+//     $('header').css('height', $(window).height());
+//     if ( ($(window).height() / $(window).width()) < (9/16) ) {
+//       $('header video').css('width','100%')
+//     } else {
+//       $('header video').css('height','100%')
+//     };
+// });
+
+$(document).ready(function() {
+    $('.player').mb_YTPlayer();
 });
 
 $('.works li').click(function(){
